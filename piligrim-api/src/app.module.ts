320108641e0a202c8d93e6ea78353e6f886cf4cb
Vaagn Avanesyan <vahagn1993@gmail.com>
2020-02-02 03:04:23 +0300

    Add modules

--- piligrim-api/src/app.module.ts
+++ piligrim-api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApiModule } from './api/api.module';
+import { SiteParserModule } from './site-parser/site-parser.module';
 
 @Module({
-  imports: [],
+  imports: [ApiModule, SiteParserModule],
   controllers: [AppController],
   providers: [AppService],
 })
