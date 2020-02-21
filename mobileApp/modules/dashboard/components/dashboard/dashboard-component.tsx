09b14a509f5bb733ccc146ff44d895e9dc88c4c2
Vaagn Avanesyan <vahagn1993@gmail.com>
2020-02-22 00:33:55 +0300

    Draft main page

--- mobileApp/modules/dashboard/components/dashboard/dashboard-component.tsx
+++ mobileApp/modules/dashboard/components/dashboard/dashboard-component.tsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import Carousel, { ParallaxImage } from 'react-native-snap-carousel';
 import { Dimensions, View, Text, StyleSheet, Platform, TouchableOpacity, ScrollView, Image } from 'react-native';
 
-const { width: screenWidth } = Dimensions.get('window')
+const { width, height } = Dimensions.get('window')
 
 export class Dashboard extends Component<any, any> {
   constructor(props) {
@@ -12,7 +12,6 @@ export class Dashboard extends Component<any, any> {
       slider: []
     }
   }
-
   componentDidMount() {
     fetch("https://piligrim-app.herokuapp.com/api/dashboard")
       .then(res => res.json())
@@ -21,15 +20,15 @@ export class Dashboard extends Component<any, any> {
       })
   }
 
-  _renderItem({ item, index }, parallaxProps) {
+  renderSlide({ item, index }, parallaxProps) {
     return (
       <TouchableOpacity onPress={() => alert(item.link)}>
-        <View style={styles.item}>
+        <View style={styles.slide}>
           <ParallaxImage
             source={{ uri: item.image }}
-            containerStyle={styles.imageContainer}
-            style={styles.image}
-            parallaxFactor={0.4}
+            containerStyle={styles.slideContainer}
+            style={styles.slideImage}
+            parallaxFactor={0.75}
             {...parallaxProps}
           />
         </View>
@@ -39,22 +38,31 @@ export class Dashboard extends Component<any, any> {
 
   render() {
     return (
-      <ScrollView>
+      <ScrollView style={styles.background}>
         <Carousel
-          sliderWidth={screenWidth}
-          sliderHeight={screenWidth}
-          itemWidth={screenWidth - 60}
+          sliderWidth={width}
+          sliderHeight={width}
+          itemWidth={width - 60}
           data={this.state.slider}
-          renderItem={this._renderItem}
+          renderItem={this.renderSlide}
           hasParallaxImages={true}
         />
-        <View>
+        <View style={styles.gallery}>
           {
-            this.state.films.map(film => <Image
-              key={film.name}
-              style={{ width: 100, height: 200 }}
-              source={{ uri: film.poster }}
-            />)
+            this.state.films.map(film =>
+              <TouchableOpacity
+                onPress={() => alert(film.link)}
+                key={film.name}
+                style={styles.film}>
+                <Image
+                  style={styles.filmPoster}
+                  source={{ uri: film.poster }}
+                />
+                <Text style={styles.filmName}>{film.name}</Text>
+                <Text style={styles.filmDuration}>{film.duration / 60} мин</Text>
+                <Text style={styles.filmGenre}>{film.genre}</Text>
+              </TouchableOpacity>
+            )
           }
         </View>
       </ScrollView>
@@ -62,20 +70,49 @@ export class Dashboard extends Component<any, any> {
   }
 }
 
-
 const styles = StyleSheet.create({
-  item: {
-    width: screenWidth - 60,
-    height: screenWidth - 60,
+  background: {
+    backgroundColor: '#070215',
   },
-  imageContainer: {
+  slide: {
+    width: width - 60,
+    height: height / 4,
+  },
+  slideContainer: {
     flex: 1,
     marginBottom: Platform.select({ ios: 0, android: 1 }), // Prevent a random Android rendering issue
-    backgroundColor: 'white',
+    backgroundColor: '#070215',
     borderRadius: 8,
   },
-  image: {
+  slideImage: {
     ...StyleSheet.absoluteFillObject,
-    resizeMode: 'cover',
+    resizeMode: "contain",
+  },
+  gallery: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    marginTop: 10
+  },
+  film: {},
+  filmPoster: {
+    width: width * .45,
+    height: width * .45,
+    marginTop: 10,
+    marginLeft: 5,
+    marginRight: 5
+  },
+  filmName: {
+    color: '#9B528E',
+    fontSize: 18, //fix issue with long strings
+  },
+  filmDuration: {
+    color: '#7992D2',
+    fontSize: 20
+  },
+  filmGenre: {
+    color: '#C7BDDF',
+    fontSize: 16,
   },
 })
