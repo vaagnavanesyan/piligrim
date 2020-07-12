8fb9f3947daeebefc2e13ca43e308a8a23e87b28
Vaagn Avanesyan <vahagn1993@gmail.com>
2020-07-12 16:11:31 +0300

    Rewrite to function

--- src/app/components/movie-feed-item.tsx
+++ src/app/components/movie-feed-item.tsx
@@ -1,39 +1,34 @@
 import React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export class MovieFeedItem extends React.Component<any> {
-  shouldComponentUpdate() {
-    return false;
-  }
+export const MovieFeedItem = (props: any) => {
+  const handleClick = async () => {
+    props.navigation.push('Movie', { movie: props.movie });
+  };
 
-  async handleClick() {
-    this.props.navigation.push('Movie', { movie: this.props.movie });
-  }
-  render() {
-    return (
-      <TouchableOpacity
-        style={styles.movie}
-        onPress={() => this.handleClick()}
-        key={this.props.movie.name}>
-        <Image
-          width={128}
-          height={128}
-          source={{ uri: this.props.movie.poster }}
-          style={styles.moviePoster}
-        />
-        <View style={styles.movieTextContainer}>
-          <Text style={styles.movieName}>{this.props.movie.name}</Text>
-          <Text style={styles.movieGenre}>{this.props.movie.genre}</Text>
-        </View>
-        <View>
-          <Text style={styles.movieDuration}>
-            {this.props.movie.duration / 60} мин
-          </Text>
-        </View>
-      </TouchableOpacity>
-    );
-  }
-}
+  return (
+    <TouchableOpacity
+      style={styles.movie}
+      onPress={() => handleClick()}
+      key={props.movie.name}>
+      <Image
+        width={128}
+        height={128}
+        source={{ uri: props.movie.poster }}
+        style={styles.moviePoster}
+      />
+      <View style={styles.movieTextContainer}>
+        <Text style={styles.movieName}>{props.movie.name}</Text>
+        <Text style={styles.movieGenre}>{props.movie.genre}</Text>
+      </View>
+      <View>
+        <Text style={styles.movieDuration}>
+          {props.movie.duration / 60} мин
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   movie: {
